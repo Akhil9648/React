ddc0ed18789afe58c052287b27846dfa1920e6ed
Akhil Pandey <akhilpandey494@gmail.com>
2025-09-07 06:41:26 +0000

    Initial Commit

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -3,10 +3,23 @@ import Footer from "./Footer/Footer";
 import Conainer from "./container/continer";
 import Logoutbutton from "./Header/Logoutbutton";
 import Logo from "./Logo";
+import RTE  from "./RTE";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import PostForm from "./post-form/Postform";
+import PostCard from "./Postcard";
+import AuthLayout from "./AuthLayout";
 export {
     Header,
     Footer,
     Conainer,
     Logo,
-    Logoutbutton
+    Logoutbutton,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    
 }
